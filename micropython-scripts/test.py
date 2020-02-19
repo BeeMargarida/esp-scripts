b774f2f88e947fe1ab40c12c590e4575a8543bba
beemargarida <up201505505@gcloud.fe.up.pt>
2020-02-19 16:04:44 +0000

    micropython scripts

---
+++ micropython-scripts/test.py
@@ -0,0 +1,20 @@
+def exec():
+    print("Hello!")
+    print("1")
+    print("1")
+    print("1")
+    print("1")
+    print("1")
+    print("1")
+    print("1")
+    a = 1 + 2
+    b = a + 1
+    print(a)
+    print(b)
+
+    if a == 1:
+        print("A")
+    elif b == 3:
+        print("Boo")
+    else:
+        print("YEE")
