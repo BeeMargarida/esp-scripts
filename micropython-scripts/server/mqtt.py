ee3c7bcb3591f7510fc1e9585d5583b6b9504131
beemargarida <up201505505@gcloud.fe.up.pt>
2020-02-25 09:37:24 +0000

    publishing in MQTT queue

--- micropython-scripts/server/mqtt.py
+++ micropython-scripts/server/mqtt.py
@@ -1,5 +1,6 @@
 from mqtt_library import MQTTClient
 import machine
+import time
 
 last_message = 0
 message_interval = 5
@@ -10,14 +11,20 @@ def sub_cb(topic, msg):
   if topic == b'notification' and msg == b'received':
     print('ESP received hello message')
 
-def connect_and_subscribe():
-  global client_id, mqtt_server, topic_sub
+def connect(mqtt_server, client_id):
   client = MQTTClient(client_id, mqtt_server)
-  client.set_callback(sub_cb)
   client.connect()
-  client.subscribe(topic_sub)
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  print('Connected to %s MQTT broker' % (mqtt_server))
   return client
+
+# def connect_and_subscribe():
+#   global client_id, mqtt_server, topic_sub
+#   client = MQTTClient(client_id, mqtt_server)
+#   client.set_callback(sub_cb)
+#   client.connect()
+#   client.subscribe(topic_sub)
+#   print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+#   return client
 
 
 def restart_and_reconnect():
@@ -25,12 +32,11 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
-def start():
+def start(mqtt_server, client_id):
     try:
-        client = connect_and_subscribe()
+        client = connect(mqtt_server, client_id)
     except OSError as e:
         restart_and_reconnect()
-
     return client
 
     # while True:
