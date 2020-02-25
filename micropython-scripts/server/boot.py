5780144519a26c6ad8a00015c452426ba87a2bcd
beemargarida <up201505505@gcloud.fe.up.pt>
2020-02-25 09:12:41 +0000

    added mqtt

--- micropython-scripts/server/boot.py
+++ micropython-scripts/server/boot.py
@@ -12,6 +12,12 @@ gc.collect()
 
 import wifi
 import server
+import mqtt
+
+mqtt_server = '192.168.1.179:1883'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_pub = b'results'
 
 wifi.connect()
-server.start()
+mqtt_client = mqtt.start()
+server.start(mqtt_client, topic_pub)
