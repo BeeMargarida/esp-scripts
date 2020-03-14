74e7edc341e58cc98e30d8508382cb830bcf3b30
beemargarida <up201505505@gcloud.fe.up.pt>
2020-03-14 17:11:47 +0000

    fixes

--- micropython-scripts/server/boot.py
+++ micropython-scripts/server/boot.py
@@ -1,8 +1,7 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import uos, machine
-import ubinascii
+import uos
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 #import webrepl
@@ -13,12 +12,8 @@ gc.collect()
 
 import wifi
 import server
-import mqtt
 
-mqtt_server = '10.250.7.209' #'192.168.1.179'
-client_id = ubinascii.hexlify(machine.unique_id())
 topic_pub = b'results'
 
 wifi.connect()
-mqtt_client = mqtt.start(mqtt_server, client_id)
-server.start(mqtt_client, topic_pub)
+server.start(topic_pub)
