fd8cd1d74bfdbf25adb39364ed0b185712dca378
beemargarida <up201505505@gcloud.fe.up.pt>
2020-04-25 18:07:03 +0100

    Wifi script

---
+++ micropython-scripts/server/wifi.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import uasyncio as asyncio
+
+def connect():
+    led = Pin(33, Pin.OUT)
+    led.on()
+
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('--', '--')
+        # sta_if.connect('raspberrypi', 'UJr2016#')
+        while not sta_if.isconnected():
+            # await asyncio.sleep(0.5)
+            pass
+
+    print('network config:', sta_if.ifconfig())
+    led.off()
