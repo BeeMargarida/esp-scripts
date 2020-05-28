0dc11de79b146139b07adf9d01a3bc02d175fc49
beemargarida <up201505505@gcloud.fe.up.pt>
2020-05-28 23:33:35 +0100

    Started support for announcement

--- micropython-scripts/server/wifi.py
+++ micropython-scripts/server/wifi.py
@@ -11,7 +11,7 @@ def connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('--', '--')
+        sta_if.connect('Calou oh puto do andar de cima', 'primodowilson')
         # sta_if.connect('raspberrypi', 'UJr2016#')
         while not sta_if.isconnected():
             # await asyncio.sleep(0.5)
