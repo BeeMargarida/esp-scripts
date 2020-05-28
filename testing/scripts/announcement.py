f4bbb8d8f5b1e36460f998868b9f8704ae624573
beemargarida <up201505505@gcloud.fe.up.pt>
2020-05-28 23:56:33 +0100

    Announcement script

---
+++ testing/scripts/announcement.py
@@ -0,0 +1,47 @@
+import sys
+import ujson
+import ubinascii
+import utime
+if sys.platform != "linux":
+    import esp
+    from machine import unique_id
+    from mqtt_as import config, MQTTClient
+else:
+    from mqtt_as import MQTTClient
+    from config import config
+if sys.platform != "linux":
+    import logging
+
+class Announcer():
+
+    def __init__(self, client_id, capabilities):
+        print("Starting up announcer...")
+        self.mqtt_client = None
+        self.mqtt_server = 'mosquitto'  # '10.250.7.209'
+        self.client_id = client_id
+        self.capabilities = capabilities
+
+        config['ssid'] = '-'
+        config['wifi_pw'] = '-'
+        config['server'] = self.mqtt_server
+        config['port'] = 1883
+        if sys.platform != "linux":
+            self.mqtt_client = MQTTClient(config)
+        else:
+            MQTTClient.DEBUG = True
+            config['client_id'] = ubinascii.hexlify(client_id)
+            self.mqtt_client = MQTTClient(**config)
+            print(str(config))
+
+        if sys.platform != "linux":
+            logging.basicConfig(level=logging.INFO)
+
+    async def run(self):
+        data = dict(
+            address=self.mqtt_client._addr,
+            capabilities=self.capabilities
+        )
+        payload = dict(
+            payload=data
+        )
+        await self.mqtt_client.publish("announcements", ujson.dumps(payload), qos = 1)
