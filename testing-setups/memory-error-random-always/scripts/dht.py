ad36f9f963a673c521f661639e7825c78de88f11
beemargarida <up201505505@gcloud.fe.up.pt>
2020-06-06 21:30:08 +0100

    Setup for memory tests

---
+++ testing-setups/memory-error-random-always/scripts/dht.py
@@ -0,0 +1,13 @@
+class DHT22():
+
+    def __init__(self, pin):
+        return
+
+    def measure(self):
+        return 
+
+    def temperature(self):
+        return 30
+
+    def humidity(self):
+        return 30
